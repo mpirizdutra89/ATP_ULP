3c71293a09b678dce1d9deeac58a2e99dbe555bc
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-10-24 19:06:33 -0300

    inicio

---
+++ src/controlador/Main.java
@@ -0,0 +1,17 @@
+
+package controlador;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
